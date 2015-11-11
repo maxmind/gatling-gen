dd7b7b5868c5ab3a1a725e5e07c843526e93d183
Ran Eilam <reilam@maxind.com>
2015-11-10 19:26:05 -0800

    2

--- shared/src/test/scala/vandegraaf/GeneratorTests.scala
+++ shared/src/test/scala/vandegraaf/GeneratorTests.scala
@@ -1,15 +1,36 @@
 package vandegraaf
 
 import org.scalacheck.Gen
+import org.scalacheck.Gen.Parameters
 import utest._
 
+import scala.util.Random
+
 object GeneratorTests extends TestSuite {
+
   def tests = TestSuite {
-    "RNGs are seeded correctly"-{
-      val gen1 = Gen.choose(1, 3);
-      val gen2 = Gen.choose(1, 3);
-      assert(1 == 1);
-//      assert(gen1.g
+
+    "RNGs are seeded correctly" - {
+
+      val makeRng = () ⇒ new Random(0)
+      val commonRng = makeRng()
+
+      // Make a list of generators, with an RNG that is either common or unique
+      val makeGenerator = (useCommon: Boolean) ⇒
+        (1 to 100) map
+        (_ ⇒ Gen.choose(0, 1000) apply
+             Parameters.default.withRng(
+               if (useCommon) commonRng else makeRng()))
+
+      val (fromCommon, fromUnique) = (makeGenerator(true), makeGenerator(false))
+      val commonHead = fromCommon.head
+
+      assert(commonHead.isDefined)
+      assert(commonHead == fromUnique.head)
+      assert(
+        ((1 to fromCommon.size) map (_ ⇒ commonHead)) ==
+        fromUnique)
+      assert(fromCommon != fromUnique)
     }
   }
 }
